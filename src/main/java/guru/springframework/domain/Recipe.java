90d3ed555c78a5c7bb48b5c35db7abdbca8bf3df
Grant Murphy <grantsmurphy@gmail.com>
2019-03-04 11:25:13 -0500

    setting up test environment

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -19,7 +19,7 @@ public class Recipe {
     private Integer cookTime;
     private Integer servings;
     private String source;
-    private String ur;
+    private String url;
 
     @Lob
     private String directions;
@@ -29,32 +29,29 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
-
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
     @ManyToMany
     @JoinTable(name = "recipe_category",
-       joinColumns = @JoinColumn(name = "recipe_id"),
+            joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-
     public void setNotes(Notes notes) {
-        this.notes = notes;
-        notes.setRecipe(this);
+        if (notes != null) {
+            this.notes = notes;
+            notes.setRecipe(this);
+        }
     }
 
-    public Recipe addIngredient(Ingredient ingredient) {
+    public Recipe addIngredient(Ingredient ingredient){
         ingredient.setRecipe(this);
         this.ingredients.add(ingredient);
         return this;
-    }
-
-    public Set<Ingredient> getIngredients() {
-        return ingredients;
     }
 }

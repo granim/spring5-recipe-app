1120628650cd94f848747ea9382cafc83d459c6c
Grant Murphy <grantsmurphy@gmail.com>
2019-03-05 12:23:11 -0500

    created ingredient view

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -4,10 +4,8 @@ import guru.springframework.commands.RecipeCommand;
 import guru.springframework.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class RecipeController {
@@ -18,12 +16,14 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/new")
     public String newRecipe(Model model) {
      model.addAttribute("recipe", new RecipeCommand());
@@ -31,13 +31,12 @@ public class RecipeController {
      return "recipe/recipeform";
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeform";
     }
-
-
 
     @PostMapping
     @RequestMapping("recipe")
@@ -45,6 +44,16 @@ public class RecipeController {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
         return "redirect:/recipe/show/" + savedCommand.getId();
     }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id) {
+        //log.debug("Deleteing id: " + id);
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
+    }
+
+
 
 
 }
---
+++ src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -0,0 +1,53 @@
+package guru.springframework.controllers;
+
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.service.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class IngredientControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController controller;
+
+    MockMvc mockmvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        controller = new IngredientController( recipeService);
+        mockmvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testListIngredients() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //when
+        mockmvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+         //then
+        verify(recipeService, times(1)).findCommandById(anyLong());
+
+
+    }
+
+
+
+
+}

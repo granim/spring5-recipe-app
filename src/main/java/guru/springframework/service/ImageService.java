516d0448517030dcc0d525fede2458b8227ac116
Grant Murphy <grantsmurphy@gmail.com>
2019-03-15 22:03:41 -0400

    implement service and Controller for uploading images

--- src/main/java/guru/springframework/service/ImageService.java
+++ src/main/java/guru/springframework/service/ImageService.java
@@ -1,4 +1,9 @@
 package guru.springframework.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+
 }
